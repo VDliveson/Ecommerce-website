85e443f22f69a9950157c22378d9ecefc7b87c41
vanshaj duggal <VDliveson>
2022-03-01 22:15:13 +0530

    Added import export functionality

--- store/admin.py
+++ store/admin.py
@@ -1,15 +1,34 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 from . models import *
 
-
-class AdminProduct(admin.ModelAdmin):
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
+        
+class CustomerResource(resources.ModelResource):
+    class Meta:
+        model = Customer
+        skip_unchanged = True
+        report_skipped = False
+        fields = ('id', 'username', 'price','email','address','phone')
+    
+class AdminProduct(ImportExportModelAdmin):
+    resource_class = ProductResource
     list_display = ['id', 'name','price','category','category_id']
     
 class AdminCategory(admin.ModelAdmin):
     list_display=['name','id']
+   
+class AdminCustomer(ImportExportModelAdmin):
+    resource_class = CustomerResource
+    list_display = ['id', 'username','email','password','phone']
+
+
     
 
-admin.site.register(Customer)
+admin.site.register(Customer,AdminCustomer)
 admin.site.register(Product,AdminProduct)
 admin.site.register(Category,AdminCategory)
 admin.site.register(Order)
